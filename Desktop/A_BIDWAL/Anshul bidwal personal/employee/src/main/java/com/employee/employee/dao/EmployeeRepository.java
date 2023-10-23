ec6e302b54a08125add53ec56b85100ceb38d9af
abidwal <bidansh007@gmail.com>
2023-10-23 19:31:21 +0530

    first commit

---
+++ Desktop/A_BIDWAL/Anshul bidwal personal/employee/src/main/java/com/employee/employee/dao/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package com.employee.employee.dao;
+
+
+
+import com.employee.employee.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+  // that's it ... no need to write any code LOL!
+  public List<Employee> findAllByOrderByLastNameAsc();
+}
---
+++ Desktop/A_BIDWAL/Anshul bidwal personal/employee/src/main/java/com/employee/employee/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.employee.employee.service;
+
+
+import com.employee.employee.entity.Employee;
+
+import java.util.List;
+
+
+public interface EmployeeService {
+
+  List<Employee> findAll();
+
+  Employee findById(int theId);
+
+  void save(Employee theEmployee);
+
+  void deleteById(int theId);
+
+}
